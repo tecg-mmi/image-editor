e1b413d9abb49d074e285404d7fe15f249b0db9d
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-03-31 11:23:41 +0200

    :tada: Initial commit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,36 @@
-console.log('Bonjour');
+import {Canvas} from "../../../canvas-framework-23/src/ts/Canvas";
+import {settings} from "./settings";
+
+let file;
+const reader = new FileReader();
+const image = new Image();
+
+function loadImage(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files.length === 1) {
+        file = target.files[0];
+        reader.readAsDataURL(file);
+    } else {
+        alert("Vous devez charger au moins une image");
+    }
+
+}
+
+function main() {
+    const canvasElement: HTMLCanvasElement = document.getElementById('my-canvas') as HTMLCanvasElement;
+    const canvas: Canvas = new Canvas(canvasElement, {width: settings.canvas.width, height: settings.canvas.height});
+    const input: HTMLInputElement = document.getElementById('upload-img') as HTMLInputElement;
+
+    input.addEventListener('change', loadImage);
+    reader.addEventListener('loadend', (event: ProgressEvent) => {
+        console.log(reader.result);
+        image.src = reader.result as string;
+    });
+    image.addEventListener('load', () => {
+        canvas.height = image.height;
+        canvas.width = image.width;
+    });
+
+}
+
+main();
---
+++ src/ts/settings.ts
@@ -0,0 +1,6 @@
+export const settings = {
+    canvas: {
+        width: 500,
+        height: 300
+    }
+}
