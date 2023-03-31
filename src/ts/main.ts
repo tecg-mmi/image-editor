1542c26eec64edc3008733b0dd7a30471d469431
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-03-31 11:43:27 +0200

    :tada: Initial commit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,8 +2,13 @@ import {Canvas} from "../../../canvas-framework-23/src/ts/Canvas";
 import {settings} from "./settings";
 
 let file;
+
+const canvasElement: HTMLCanvasElement = document.getElementById('my-canvas') as HTMLCanvasElement;
+const canvas: Canvas = new Canvas(canvasElement, {width: settings.canvas.width, height: settings.canvas.height});
+const input: HTMLInputElement = document.getElementById('upload-img') as HTMLInputElement;
 const reader = new FileReader();
 const image = new Image();
+let imageLoaded = false;
 
 function loadImage(event: Event) {
     const target = event.target as HTMLInputElement;
@@ -13,30 +18,36 @@ function loadImage(event: Event) {
     } else {
         alert("Vous devez charger au moins une image");
     }
-
 }
 
-function main() {
-    const canvasElement: HTMLCanvasElement = document.getElementById('my-canvas') as HTMLCanvasElement;
-    const canvas: Canvas = new Canvas(canvasElement, {width: settings.canvas.width, height: settings.canvas.height});
-    const input: HTMLInputElement = document.getElementById('upload-img') as HTMLInputElement;
-
+function addEventListeners() {
     input.addEventListener('change', loadImage);
     reader.addEventListener('loadend', (event: ProgressEvent) => {
         console.log(reader.result);
         image.src = reader.result as string;
     });
+
     image.addEventListener('load', () => {
         if (image.width > settings.canvas.maxWidth || image.height > settings.canvas.maxHeight) {
             alert(`L'image est trop grande max(l${settings.canvas.maxWidth},h${settings.canvas.maxHeight})`);
+            imageLoaded = false;
         } else {
             canvas.height = image.height;
             canvas.width = image.width;
 
         }
         canvas.ctx.drawImage(image, 0, 0);
+        imageLoaded = true;
     });
+    canvas.canvasElement.addEventListener('click', () => {
+        if (!imageLoaded) {
+            input.click();
+        }
+    });
+}
 
+function main() {
+    addEventListeners();
 }
 
 main();
