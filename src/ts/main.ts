66a49c9f39ab3a1d3d56ceabcc9780abf2617bc2
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-04-01 13:38:14 +0200

    :tada: Initial commit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,17 +1,20 @@
 import {Canvas} from "../../../canvas-framework-23/src/ts/Canvas";
 import {settings} from "./settings";
+import {Filter} from "./Filter";
 
 let file;
 
 const canvasElement: HTMLCanvasElement = document.getElementById('my-canvas') as HTMLCanvasElement;
 const canvas: Canvas = new Canvas(canvasElement, {width: settings.canvas.width, height: settings.canvas.height});
 const input: HTMLInputElement = document.getElementById('upload-img') as HTMLInputElement;
+const opacityInput: HTMLInputElement = document.getElementById('opacity') as HTMLInputElement;
 const reader = new FileReader();
 const image = new Image();
 let imageLoaded = false;
+const filters: Filter[] = [];
 
 function loadImage(event: Event) {
-    const target = event.target as HTMLInputElement;
+    const target = event.currentTarget as HTMLInputElement;
     if (target.files.length === 1) {
         file = target.files[0];
         reader.readAsDataURL(file);
@@ -36,7 +39,7 @@ function addEventListeners() {
             canvas.width = image.width;
 
         }
-        canvas.ctx.drawImage(image, 0, 0);
+        drawImage();
         imageLoaded = true;
     });
     canvas.canvasElement.addEventListener('click', () => {
@@ -44,10 +47,40 @@ function addEventListeners() {
             input.click();
         }
     });
+
+    opacityInput.addEventListener('input', (event) => {
+        if (imageLoaded) {
+            canvas.clear();
+            const target = event.currentTarget as HTMLInputElement;
+            for (const filter of filters) {
+                if (filter.name === target.id) {
+                    filter.value = target.value;
+                }
+            }
+            drawImage();
+        }
+    });
+
+}
+
+function loadFilters() {
+    for (const rangeInput of document.getElementsByClassName('form-range')) {
+        // @ts-ignore
+        filters.push({name: rangeInput.id, unit: rangeInput.dataset.unit, value: rangeInput.value});
+    }
+}
+
+function drawImage() {
+    canvas.ctx.filter = filters.map((filter) => {
+        return `${filter.name}(${filter.value}${filter.unit})`;
+    }).join(' ');
+    canvas.ctx.drawImage(image, 0, 0);
 }
 
 function main() {
     addEventListeners();
+    loadFilters();
+    drawImage();
 }
 
 main();
